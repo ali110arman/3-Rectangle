9ae914ec54f0798a8e7d87e9d628b0c965de89e6
ali arman <56387750+ali110arman@users.noreply.github.com>
2022-02-04 18:50:25 +0330

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+void main() {
+	int tool, arz, mohit=0, masahat=0;
+	cout << "inter hight:\n";
+	cin >> tool;
+	cout << "inter lengh:\n";
+	cin >> arz;
+	mohit = 2 * (tool + arz);
+	masahat = tool * arz;
+	cout << "your perimiter:" << mohit << " and your area:" << masahat;
+}
